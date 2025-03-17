a42784d610ea145e55f6ce3dc915267221508ba4
eunbyeol679 <eunbyeol679@naver.com>
2025-03-17 22:08:41 +0900

    v0.3 O(log n) update

--- week02.py
+++ week02.py
@@ -1,7 +1,18 @@
 # 실행: alt + shift + F10
 
-n = int(input("정수 입력 : "))
-result = 0
-result = n * (n + 1) // 2
-# 시간 복잡도 O(1) <= 상수 시간
-print(result)
+import random
+
+num = random.randint(1, 101)
+for i in range(1, 8):
+    answer = int(input("정답을 입력하세요 : "))
+    if num > answer:
+        print(f"{answer}보다 큰 수입니다.")
+    elif num < answer:
+        print(f"{answer}보다 작은 수입니다.")
+    elif num > 100 or num < 0:
+        print("1에서 100까지의 정수를 입력하세요.")
+    else:
+        print("정답입니다!")
+        break
+
+
