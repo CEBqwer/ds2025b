f995d8835bc4abb430f827a03e313558705c8246
eunbyeol679 <eunbyeol679@naver.com>
2025-03-24 10:51:04 +0900

    v0.5 array2

--- week03.py
+++ week03.py
@@ -1,3 +1,6 @@
-array = [11, 9, -77, 8]
-for i in range(len(array)):
-    print(f"{array[i]:3}, {id(array[i])}")
+import array
+
+arr = array.array('f', [11, 9, -77, 8]) # 튜플로 해도 동일
+for i in range(len(arr)):
+    print(f"{arr[i]:3}, {id(arr[i])}")
+print(arr[2]) # 접근은 O(1)
