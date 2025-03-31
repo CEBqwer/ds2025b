0a8fd9ec936584d3eca7f5acdef283311b6c6bf4
eunbyeol679 <eunbyeol679@naver.com>
2025-03-31 11:48:30 +0900

    v1.4 Linked List

--- week04.py
+++ week04.py
@@ -1,3 +1,5 @@
+import random
+
 class Node:
     def __init__(self, data, link=None):
         self.data = data
@@ -17,6 +19,15 @@ class LinkedList:
             current = current.link # 다음 노드로 이동
         current.link = Node(data)
 
+    def search(self, target):
+        current = self.head
+        while current.link:
+            if current.data == target:
+                return f"{target}을(를) 찾았습니다."
+            else:
+                current = current.link # 다음 노드로 이동
+        return f"{target}은(는) 링크드 리스트 안에 존재하지 않습니다."
+
     def __str__(self):
         node = self.head
         out_texts = ""
@@ -27,9 +38,18 @@ class LinkedList:
         return out_texts + "end"
 
 
-ll = LinkedList()
-ll.append(8)
-ll.append(10)
-ll.append(-9)
+# ll = LinkedList()
+# ll.append(8)
+# ll.append(10)
+# ll.append(-9)
 
+# print(ll)
+# print(ll.search(100))
+# print(ll.search(10))
+
+ll = LinkedList()
+# for i in range(0, 20):
+for _ in range(20):
+    ll.append(random.randint(1, 50))
 print(ll)
+print(ll.search(10))
