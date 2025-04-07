12bc1f0dfc9f95f22883bd657543b022ef5e7a29
eunbyeol679 <eunbyeol679@naver.com>
2025-04-07 11:44:16 +0900

    v2.1 stack

--- week05.py
+++ week05.py
@@ -1,36 +1,14 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.link = None
-
-
-class Stack:
-    def __init__(self):
-        self.top = None
-
-
-    def push(self, data):
-        node = Node(data)
-        if self.top is None:
-            self.top = node
-        else:
-            node.link = self.top
-            self.top = node
-
-
-    def pop(self):
-        if self.top is None:
-            return "Stack is empty. Nothing to delete."
-        popped_node = self.top
-        self.top = self.top.link
-        return popped_node.data
-
-
-s1 = Stack()
+s1 = list()
+print(len(s1))
+s1.append("ds") # push
+s1.append("db")
+print(len(s1)) # size
+print(s1)
+print(s1.pop()) # pop
+print(s1[-1]) # peek
+print(s1)
 print(s1.pop())
-s1.push("Data Structure")
-s1.push("Database")
-# print(s1.pop())
-# print(s1.pop())
-for i in range(3):
-    print(s1.pop())
+print(s1)
+print(s1.pop())
+print(s1)
+
