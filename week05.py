5ed54dc4ed6166c6f9cb712e25da1281ac21ad04
eunbyeol679 <eunbyeol679@naver.com>
2025-04-14 09:22:13 +0900

    v2.3 stack

--- week05.py
+++ week05.py
@@ -1,17 +1,31 @@
-def check_parentheses(expression : str) -> bool: # type hint
-    stack = []
-    for letter in expression:
-        if letter == "(":
-            stack.append(letter)
-        if letter == ")":
-            if len(stack) == 0:
-                return False
-            else:
-                stack.pop()
-    return len(stack) == 0
+class Minstack():
+    def __init__(self):
+        self.main = [] # push, pop
+        self.min = [] # 가장 작은 요소를 항상 추적
+
+    def push(self, n):
+        if len(self.main) == 0: # main 비어 있으면 n은 무조건 가장 작은 값
+            self.min.append(n)
+        elif n <= self.min[-1]: # min의 마지막 요소는 항상 스택에서 가장 작은 값
+            self.min.append(n)
+        else: # min의 마지막 요소보다 크면
+            self.min.append(self.min[-1])
+        self.main.append(n)
+
+    def pop(self):
+        self.min.pop()
+        return self.main.pop()
+
+    def get_min(self):
+        return self.min[-1]
 
 
-print(check_parentheses("(2*3)"))
-print(check_parentheses("(2*3"))
-print(check_parentheses("(2+(3*9)")) # 스택에 여는 소괄호 하나 남아 있어서 False
-print(check_parentheses(")(2*3)("))
+min_stack = Minstack()
+min_stack.push(10)
+print(min_stack.main)
+print(min_stack.min)
+
+min_stack.push(30)
+print(min_stack.main)
+print(min_stack.min)
+print(min_stack.get_min())
