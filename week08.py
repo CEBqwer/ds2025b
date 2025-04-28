251827c7b8a0c8720ccb03426219954afd359498
eunbyeol679 <eunbyeol679@naver.com>
2025-04-28 11:43:15 +0900

    v2.9 tree

--- week08.py
+++ week08.py
@@ -1,3 +1,27 @@
+def pre_order(node): # 재귀함수 사용
+    if node is None:
+        return
+    print(node.data, end = '-')
+    pre_order(node.left)
+    pre_order(node.right)
+
+
+def in_order(node):
+    if node is None:
+        return
+    in_order(node.left)
+    print(node.data, end='-')
+    in_order(node.right)
+
+
+def post_order(node):
+    if node is None:
+        return
+    post_order(node.left)
+    post_order(node.right)
+    print(node.data, end='-')
+
+
 class TreeNode:
 	def __init__(self):
 		self.left = None
@@ -29,3 +53,10 @@ node3.left = node6
 
 print(node6.data)
 print(node1.right.left.data)
+
+pre_order(node1)
+print()
+in_order(node1)
+print()
+post_order(node1)
+print()
